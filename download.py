7850a77bf0ce192ccffb97640bfaa3978fee1f17
Anaya Koirala <anayakoirala@protonmail.com>
2022-10-25 14:47:57 +0545

    inital commit

---
+++ download.py
@@ -0,0 +1,44 @@
+import pytube
+
+def error():
+    print("Error Downloading :(")
+
+class SearchDownload:
+    def __init__(self,query):
+        self.query = query
+        self.results = pytube.Search(query)
+    
+    def get_data(self):
+        for result in self.results.results:
+            return result.video_id,result.title, result.author , result.length
+    
+class PlaylistDownload:
+    def __init__(self,link):
+        self.link = link
+        self.yt = pytube.Playlist(self.link)
+        self.title = self.yt.title
+
+    def download_video_default(self):
+        for video in self.yt.videos:
+            video.streams.get_highest_resolution().download()
+
+    def download_audio_default(self):
+        for video in self.yt.videos:
+            video.streams.filter(abr="128kbps",progressive=False).first().download()
+
+class SingleDownload:
+    def __init__(self,link):
+        self.link = link
+        self.yt = pytube.YouTube(self.link)
+        self.title = self.yt.title
+        self.author = self.yt.author
+        self.date = self.yt.publish_date.strftime("%Y-%m-%d")
+        self.length = self.yt.length
+        self.yt = pytube.Playlist(self.link)
+          
+    def download_video_default(self):
+        self.yt.streams.get_highest_resolution().download()
+    
+    def download_audio_default(self):
+        self.yt.streams.filter(abr="128kbps",progressive=False).first().download()
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+1) Make a simple form to input wheather user would like to download single video or playlist
+2) Then another simple form to input wheather user want video or audio 
+3) Add that image too , it looks cool
+4) No js , templates or other bloat; Pure HTML; minimal CSS
+
